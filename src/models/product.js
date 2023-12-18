d9f588ee9065e24ad5cab35d448a5e7c96651fc1
KaungMinKhant <kaungminkhant.edka@gmail.com>
2023-12-18 09:31:31 +0630

    product order cart

---
+++ src/models/product.js
@@ -0,0 +1,54 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Product.init({
+    product_id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID
+    },
+    category_id: {
+      type: DataTypes.UUID,
+      allowNull:false
+    },
+    name: {
+      type: DataTypes.STRING
+    },
+    description: {
+      type: DataTypes.STRING
+    },
+    quantity: {
+      type: DataTypes.STRING
+    },
+    buy_price: {
+      type: DataTypes.STRING
+    },
+    sell_price: {
+      type: DataTypes.STRING
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }, {
+    sequelize,
+    modelName: 'Product',
+  });
+  return Product;
+};
